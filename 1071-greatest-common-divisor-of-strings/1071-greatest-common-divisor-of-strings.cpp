40cc78044180512fd0c6b34427e88c0866debb5a
RAHUL SIRVI <106471924+RAHULSIRVI1101@users.noreply.github.com>
2022-12-01 21:23:04 +0530

    Time: 7 ms (57.16%), Space: 7.2 MB (34.38%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2!=str2+str1) return "";
+        int gcd=__gcd(str1.length(),str2.length());
+        return str2.substr(0,gcd);
+    }
+};
